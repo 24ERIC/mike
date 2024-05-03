8906181f720a6a13d1472aad147ec7e29ec852bd
24ERIC <60751655+24ERIC@users.noreply.github.com>
2024-05-03 17:48:06 +0800

    Update ImageComponent.js

--- gpt-image-client/src/ImageComponent.js
+++ gpt-image-client/src/ImageComponent.js
@@ -6,7 +6,7 @@ const ImageComponent = () => {
 
     const fetchImage = async () => {
         try {
-            const response = await axios.get('http://localhost:5000/get-image');
+            const response = await axios.get('https://mike-five.vercel.app/get-image');
             setImage(response.data.image);
         } catch (error) {
             console.error('Error fetching image:', error);
